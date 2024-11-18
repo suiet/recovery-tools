f51f40ee1fdd51a6d5fd8d1f3462025d667d6199
brrruski <brrruski@suiet.app>
2024-11-18 11:48:29 -0500

    ci: ignore ts typing error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Buffer } from "buffer";
-import { Account, GlobalMeta, SuietBackup, TokenTool } from "./core";
+import { GlobalMeta, SuietBackup, TokenTool } from "./core";
 import { decryptMnemonic, decryptPrivateKey } from "./core/crypto";
 import { SignatureScheme, SupportedEncoding } from "./core/SuiKeypairUtil";
 import { SuiKeypairUtil } from "./core/SuiKeypairUtil";
